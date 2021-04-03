1eb514dc76bf0d5c053684a311ceef3d446d532d
bhagyashreep25 <bagsphadnis@gmail.com>
2021-04-03 21:30:30 +0530

    merging changes

--- frontend/ui/src/App.js
+++ frontend/ui/src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+import Card from "react-bootstrap/Card";
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+          <Navbar.Brand href="#">Image</Navbar.Brand>
+        </Navbar>
+        <Container>
+          <Row>
+            <Col sm={6}>sm=8</Col>
+            <Col sm={6}>
+              <Card>
+                <Card.Body>Content</Card.Body>
+              </Card>
+            </Col>
+          </Row>
+      </Container>
+      
       </header>
-    </div>
+    // </div>
   );
 }
 
