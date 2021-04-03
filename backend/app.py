364ef0c5c382dab948b242d07048588104de33af
Amanbeer Khanduja <53832573+SockAndSandal@users.noreply.github.com>
2021-04-04 02:02:22 +0530

    updated flask api

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,30 @@
 import pandas as pd
 from flask import Flask, jsonify, request
-import pickle
+import numpy as np
+from PIL import Image
+import base64
+import re
+from io import StringIO, BytesIO
+from tensorflow import keras
+
 
 # load model
-model = pickle.load(open('model.pkl','rb'))
-
+model = model = keras.models.load_model('./model.h5')
 # app
 app = Flask(__name__)
 
-# routes
-@app.route('/', methods=['POST'])
 
+
+# routes
+@app.route('/maskImage' , methods=['POST'])
+def maskImage():
+    base64_img = file = request.files['canvas'].read() 
+    base64_img = re.sub('^data:image/.+;base64,', '', base64_img)
+    base64_img_bytes = base64_img.encode('utf-8')
+    decoded_image_data = base64.decodebytes(base64_img_bytes)
+    return decoded_image_data
+
+@app.route('/', methods=['POST'])
 def predict():
     # get data
     data = request.get_json(force=True)
